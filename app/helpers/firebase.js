130971851e3857566ec95eec5cd8808d5e438de6
Jamie Bradley <jamie.bradley@pyidigital.org>
2017-05-17 14:52:48 +0100

    updated the helpers to include a date

--- app/helpers/firebase.js
+++ app/helpers/firebase.js
@@ -4,25 +4,28 @@
  * against a Firebase instance.
  */
 const firebase = require('firebase')
+const moment = require('moment')
 
 class FirebaseHelpers {
-  getData(table) {
+  getData (table) {
     return firebase.database().ref(`${table}/`).once('value')
   }
 
-  postData(table, payload) {
+  postData (table, payload) {
     const tableRef = firebase.database().ref(`${table}/`)
+    const date = new Date()
 
     // Assign timestamp to the payload
-    payload.timestamp = Number(new Date())
+    payload.timestamp = moment(date).format('MM/DD/YYYY HH:mm')
+
     return tableRef.push(payload)
   }
 
-  updateData(table, id, payload) {
+  updateData (table, id, payload) {
     return firebase.database().ref(`${table}/${id}`).update(payload)
   }
 
-  deleteData(table, id) {
+  deleteData (table, id) {
     return firebase.database().ref(`${table}/${id}`).remove()
   }
 }
